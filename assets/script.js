2ded6b06f1194b091174a54537d53fb7eb5974a3
aaansari123 <asadali1029@gmail.com>
2022-08-01 18:33:45 -0400

    created page, creating button functionality for calling api

---
+++ assets/script.js
@@ -0,0 +1,55 @@
+// key 798857f5f7d6213943574838124df4c6
+var searchButton = document.querySelector("#searchButton");
+var fiveDayForecast = document.querySelector("#weather-container");
+var currentWeather = document.querySelector("#weather-forecast");
+var searchHistroy = document.querySelector(".history");
+var input = document.querySelector("#city");
+var weatherEl = document.createElement("a");
+var oldButtons = document.querySelector(".buttons");
+
+var searches = [];
+var index = 0;
+function handleSubmit(event){
+    event.preventDefault();
+    var search = input.value.trim();
+    searches.push(search);
+    console.log(searches);
+    localStorage.setItem(index, search);
+    index ++;
+    displayHistory();
+    console.log(index);
+    input.value = "";
+    getWeather(search);
+}
+
+function displayHistory(){
+        var searchEl = document.createElement("button");
+        searchEl.classList.add("buttons");
+        searchEl.innerHTML = localStorage.getItem(index-1);
+        searchHistroy.appendChild(searchEl);
+        searchEl.style.listStyle = "none";
+
+}
+
+function displayWeather(data){
+
+    console.log(data);
+    weatherEl.innerHTML = JSON.stringify(data.main);
+    currentWeather.appendChild(weatherEl);
+
+}
+function getWeather(name){
+    var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + name + "&appid=798857f5f7d6213943574838124df4c6"
+    fetch(apiURL).then(function (response){
+        if (response.ok){
+            response.json().then(function(data){
+                displayWeather(data);
+            });
+        }
+    });
+};
+
+function handleOldButtons(){
+    
+}
+searchButton.addEventListener('click', handleSubmit);
